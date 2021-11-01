24bdd61e068d30df0ab83de2e7913295da4d57d7
Misha Kutuzov <mishakutuzov2002@yandex.ru>
2021-11-01 17:14:53 +0300

    feat: added ORM

---
+++ Banks/ORM/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Banks.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banks.ORM
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
