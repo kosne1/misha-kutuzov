0ddd5724149a972f24ef742f30a17c079f69b615
Михаил <mishakutuzov2002@yandex.ru>
2021-11-08 10:10:04 +0300

    fix: deleted orm

--- Banks/UI/Entities/ConsoleService.cs
+++ Banks/UI/Entities/ConsoleService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Banks.BankAccounts;
+using Banks.Db;
 using Banks.Entities;
-using Banks.ORM;
 using Banks.UI.Services;
 
 namespace Banks.UI.Entities
