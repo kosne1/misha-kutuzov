0741020c94df6601b4c2b1c8657c5fee8e5a1c66
Михаил Кутузов <mishakutuzov2002@yandex.ru>
2021-12-20 01:48:03 +0300

    feat: created employee service

---
+++ Reports.Server/Services/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using Reports.DAL.Entities;
+
+namespace Reports.Server.Services;
+
+public interface IEmployeeService
+{
+    Employee Create(string name);
+
+    Employee FindByName(string name);
+
+    Employee FindById(Guid id);
+
+    void Delete(Guid id);
+
+    Employee Update(Employee entity);
+}
