76ed02b35f2f827ea2f43a45ffb755c2330400c1
quvi <quvivirus@vk.com>
2021-12-27 03:39:49 +0300

    fix: fixed SupervisorController.cs

--- Reports.Server/Controllers/EmployeeController.cs
+++ Reports.Server/Controllers/EmployeeController.cs
@@ -1,4 +1,3 @@
-using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Reports.DAL.Models.Employees;
 using Reports.Server.Services;
--- Reports.Server/Controllers/SupervisorController.cs
+++ Reports.Server/Controllers/SupervisorController.cs
@@ -6,13 +6,63 @@ namespace Reports.Server.Controllers;
 
 [ApiController]
 [Route("/supervisors")]
-public class SupervisorController : EmployeeController
+public class SupervisorController : ControllerBase
 {
     private readonly SupervisorService _service;
-    
-    public SupervisorController(SupervisorService service) : base(service)
+
+    public SupervisorController(SupervisorService service)
     {
         _service = service;
+    }
+    
+    [HttpPost]
+    public async Task<EmployeeModel> Create([FromQuery] string name)
+    {
+        return await _service.Create(name);
+    }
+
+    [HttpGet("name")]
+    public IActionResult FindByName([FromQuery] string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) return BadRequest();
+        EmployeeModel result = _service.FindByName(name);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+
+        return NotFound();
+    }
+
+    [HttpGet("id")]
+    public IActionResult FindById([FromQuery] Guid id)
+    {
+        if (id == Guid.Empty) return BadRequest();
+        EmployeeModel result = _service.FindById(id);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+
+        return NotFound();
+    }
+
+    [HttpGet("all")]
+    public IActionResult GetAll()
+    {
+        return Ok(_service.GetAll());
+    }
+
+    [HttpDelete]
+    public void Delete([FromQuery] Guid id)
+    {
+        _service.Delete(id);
+    }
+
+    [HttpPut]
+    public EmployeeModel Update([FromQuery] EmployeeModel entity)
+    {
+        return _service.Update(entity);
     }
 
     [HttpGet("subordinatesTasks")]
@@ -20,7 +70,7 @@ public class SupervisorController : EmployeeController
     {
         return Ok(_service.GetSubordinatesTasks(id));
     }
-    
+
     [HttpPut("subordinate")]
     public EmployeeModel AddSubordinate([FromQuery] Guid supervisorId, [FromQuery] Guid employeeId)
     {
